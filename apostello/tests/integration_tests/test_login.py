2d6d65350ee6cea97f7fccacd89a4a421686f131
Dean Montgomery <montgomery.dean97@gmail.com>
2015-11-28 11:19:48 +0000

    add tests

--- apostello/tests/integration_tests/test_login.py
+++ apostello/tests/integration_tests/test_login.py
@@ -1,28 +1,10 @@
 import pytest
-
+from time import sleep
 
 @pytest.mark.django_db
 class TestLogin:
     @pytest.mark.parametrize("uri", [
         '/',
-        '/send/adhoc/',
-        '/send/group/',
-        '/group/all/',
-        '/group/new/',
-        '/group/edit/1/',
-        '/recipient/all/',
-        '/recipient/new/',
-        '/recipient/edit/1/',
-        '/keyword/all/',
-        '/keyword/new/',
-        '/keyword/edit/1/',
-        '/keyword/responses/1/',
-        '/keyword/responses/1/archive/',
-        '/keyword/responses/csv/1/',
-        '/incoming/',
-        '/outgoing/',
-        '/incoming/curate_wall/',
-        '/incoming/curate_wall/',
     ])
     def test_not_logged_in(self, uri, live_server, browser, users):
         browser.get(live_server + uri)
@@ -32,33 +14,18 @@ class TestLogin:
 
     @pytest.mark.parametrize("uri", [
         '/',
-        '/send/adhoc/',
-        '/send/group/',
-        '/group/all/',
-        '/group/new/',
-        '/group/edit/1/',
-        '/recipient/all/',
-        '/recipient/new/',
-        '/recipient/edit/1/',
-        '/keyword/all/',
-        '/keyword/new/',
-        '/keyword/edit/1/',
-        '/keyword/responses/1/',
-        '/keyword/responses/1/archive/',
-        '/keyword/responses/csv/1/',
-        '/incoming/',
-        '/outgoing/',
-        '/incoming/curate_wall/',
-        '/incoming/curate_wall/',
     ])
     def test_log_in(self, uri, live_server, browser, users):
         # login
         browser.get(live_server + uri)
+        email_button = browser.find_elements_by_xpath('/html/body/div/div/div/button')[0]
+        email_button.click()
+        sleep(2)
         email_box = browser.find_elements_by_name('login')[0]
         email_box.send_keys(users['staff'].email)
         password_box = browser.find_elements_by_name('password')[0]
         password_box.send_keys('top_secret')
-        login_button = browser.find_elements_by_xpath('html/body/div/div/div/form/button')[0]
+        login_button = browser.find_elements_by_xpath('html/body/div/div/div/div/form/button')[0]
         login_button.click()
         # check we have been redirected
         assert live_server + uri in browser.current_url
---
+++ apostello/tests/integration_tests/test_signup.py
@@ -0,0 +1,27 @@
+import pytest
+
+from django.core import mail
+
+@pytest.mark.django_db
+class TestSignup:
+    def test_sign_up(self, live_server, browser, users):
+        """
+        Tests the sign up form and checks that the appropriate emails
+        have been sent afterwards.
+        """
+        # signup
+        uri = '/accounts/signup'
+        browser.get(live_server + uri)
+        email_box = browser.find_elements_by_name('email')[0]
+        email_box.send_keys('testsignupemail@example.com')
+        password_box1 = browser.find_elements_by_name('password1')[0]
+        password_box1.send_keys('top_secret')
+        password_box2 = browser.find_elements_by_name('password2')[0]
+        password_box2.send_keys('top_secret')
+        login_button = browser.find_elements_by_xpath('html/body/div/div/div/form/button')[0]
+        login_button.click()
+        # check we have been redirected
+        assert '/accounts/confirm-email/' in browser.current_url
+        assert len(mail.outbox) == 2
+        assert '[apostello] New User' in mail.outbox[0].subject
+        assert 'Please Confirm Your E-mail Address' in mail.outbox[1].subject
