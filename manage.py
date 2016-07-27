56629b85ed678034863e0ff2b75412fce95530ff
Dean Montgomery <montgomery.dean97@gmail.com>
2016-07-27 19:25:40 +0100

    use dev settings by default with manage.py

--- manage.py
+++ manage.py
@@ -8,6 +8,7 @@ if os.environ.get('DYNO_RAM') is None:
     loaddotenv()
 
 if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.dev")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
