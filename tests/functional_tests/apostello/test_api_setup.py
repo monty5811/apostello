a019df926d651fe64094b79c0ccb5adb94405379
Dean Montgomery <montgomery.dean97@gmail.com>
2016-08-06 15:56:27 +0100

    test deleting api key when none exists

--- tests/functional_tests/apostello/test_api_setup.py
+++ tests/functional_tests/apostello/test_api_setup.py
@@ -14,6 +14,13 @@ class TestAPISetup:
         no_api_token_txt = 'No API Token'
         b = browser_in
         browser_in.get(live_server + URI)
+        # delete token that doesn't exist
+        del_button = b.find_elements_by_xpath(
+            '/html/body/div[3]/div/div/div/div[2]/form[2]/button'
+        )[0]
+        del_button.click()
+        sleep(driver_wait_time)
+        assert no_api_token_txt in b.page_source
         # generate token for first time
         assert no_api_token_txt in b.page_source
         regen_button = b.find_elements_by_xpath(
