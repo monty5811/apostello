0f79528a16a183e7a22b1d9ece4baf990b619abe
Dean Montgomery <montgomery.dean97@gmail.com>
2016-08-16 18:20:18 +0100

    improve source pacakging

--- setup.py
+++ setup.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python
-
 import os
 import sys
+
 import pip
-
 from pip.req import parse_requirements
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import find_packages, setup
 
 version = "1.11.0"
 
 install_reqs = parse_requirements(
-    'requirements_test.txt',
+    'requirements.txt',
     session=pip.download.PipSession()
 )
 reqs = [str(ir.req) for ir in install_reqs]
 
-description = 'sms communication for churches, built with django'
+description = 'sms for your church'
 
 setup(
     name='apostello',
@@ -29,17 +24,14 @@ setup(
     author='Dean Montgomery',
     author_email='montgomery.dean97@gmail.com',
     url='https://github.com/monty5811/apostello',
-    packages=[],
+    packages=find_packages(exclude=['tests*']),
     install_requires=reqs,
     license='MIT',
     zip_safe=False,
+    include_package_data=True,
     classifiers=[
-        'Framework :: Django :: 1.9',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython',
+        "Private :: Do Not Upload"
     ],
     keywords=('Python, twilio, sms, church, django, '),
 )
+
