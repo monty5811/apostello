e7312e168028d8fba5d6b68a164f7567785d93ec
Dean Montgomery <montgomery.dean97@gmail.com>
2016-08-17 18:26:20 +0100

    tidy up response card

--- assets/js/components/response_card.jsx
+++ assets/js/components/response_card.jsx
@@ -1,29 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ResponseCard extends Component {
-  constructor() {
-    super();
-    this.onClick = this.onClick.bind(this);
-  }
-  onClick() {
-    this.props.toggleCard(this.props.response);
-  }
-  render() {
-    const resp = this.props.response;
-    const firstWord = resp.content.split(' ')[0];
-    const restOfMessage = resp.content.split(' ').splice(1).join(' ');
-    const styles = {
-      backgroundColor: resp.displayon_wall ? '#ffffff' : '#616161',
-      fontSize: !this.props.preview && !this.props.curating ? '200%' : '100%',
-    };
-    return (
-      <div className="card" onClick={this.onClick} style={styles}>
-        <div className="content">
-          <p><span style={{ color: '#D3D3D3' }}>{firstWord}</span> {restOfMessage}</p>
-        </div>
+const ResponseCard = (props) => {
+  const resp = props.response;
+  const firstWord = resp.content.split(' ')[0];
+  const restOfMessage = resp.content.split(' ').splice(1).join(' ');
+  const styles = {
+    backgroundColor: '#ffffff',
+    fontSize: '200%',
+  };
+  return (
+    <div className="card" style={styles}>
+      <div className="content">
+        <p>
+          <span style={{ color: '#D3D3D3' }}>{firstWord}</span> {restOfMessage}
+        </p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ResponseCard;
