27c50897896ddd77d1acb36efc3cdfc848e2c445
Dean Montgomery <montgomery.dean97@gmail.com>
2018-03-19 17:09:03 +0000

    nicer way to handle elm startup errors

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,5 +1,7 @@
 module Main exposing (main)
 
+import Html exposing (Html)
+import Html.Attributes as A
 import Json.Decode as Decode
 import Messages exposing (Msg(UrlChange))
 import Models exposing (Model, decodeFlags, initialModel)
@@ -10,30 +12,77 @@ import Update exposing (update)
 import View exposing (view)
 
 
-main : Program Decode.Value Model Msg
+main : Program Decode.Value (Result String Model) Msg
 main =
     Navigation.programWithFlags UrlChange
         { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
+        , view = viewResult
+        , update = updateResult
+        , subscriptions = subscriptionsResult
         }
 
 
-init : Decode.Value -> Navigation.Location -> ( Model, Cmd Msg )
+init : Decode.Value -> Navigation.Location -> ( Result String Model, Cmd Msg )
 init flagsVal location =
     let
         flags =
-            case Decode.decodeValue decodeFlags flagsVal of
-                Ok f ->
-                    f
+            Decode.decodeValue decodeFlags flagsVal
 
-                Err msg ->
-                    Debug.crash <| "Try reloading the page\n" ++ msg
+        page =
+            Result.map (.settings >> loc2Page location >> Tuple.first) flags
 
-        ( page, _ ) =
-            loc2Page location flags.settings
+        model =
+            Result.map2 (.settings >> initialModel) flags page
     in
-    page
-        |> initialModel flags.settings
-        |> update (UrlChange location)
+    updateResult (UrlChange location) model
+
+
+viewResult : Result String Model -> Html Msg
+viewResult result =
+    case result of
+        Ok model ->
+            view model
+
+        Err error ->
+            viewError error
+
+
+updateResult : Msg -> Result String Model -> ( Result String Model, Cmd Msg )
+updateResult msg result =
+    case result of
+        Ok model ->
+            let
+                ( newModel, newCmd ) =
+                    update msg model
+            in
+            ( Ok newModel, newCmd )
+
+        Err _ ->
+            ( result, Cmd.none )
+
+
+subscriptionsResult : Result String Model -> Sub Msg
+subscriptionsResult result =
+    case result of
+        Ok model ->
+            subscriptions model
+
+        Err _ ->
+            Sub.none
+
+
+viewError : String -> Html Msg
+viewError err =
+    Html.div
+        [ A.style
+            [ ( "width", "100vw" )
+            , ( "height", "100vh" )
+            , ( "background", "#5a589b" )
+            , ( "color", "#fff" )
+            , ( "text-align", "center" )
+            , ( "padding-top", "10rem" )
+            ]
+        ]
+        [ Html.h1 [] [ Html.text "Something broke there, please try reloading the page..." ]
+        , Html.p [ A.style [ ( "padding-top", "10rem" ) ] ] [ Html.text <| "Detailed error: " ++ err ]
+        ]
--- assets/js/app.js
+++ assets/js/app.js
@@ -2,22 +2,7 @@ import renderElm from './render_elm';
 
 function handleDOMContentLoaded() {
   // start elm
-  try {
-    const elmApp = renderElm();
-  } catch (e) {
-    console.error(e);
-    const node = document.getElementById('elmContainer');
-    const style =
-      'width:100vw;height:100vh;background:#5a589b;color:#fff;font-size:xx-large;';
-    node.innerHTML =
-      '<div style="' +
-      style +
-      '"><p>Something broke there, try reloading the page...</p></div>' +
-      '<br></br>' +
-      '<br></br>' +
-      '<br></br>' +
-      e;
-  }
+  const elmApp = renderElm();
 }
 
 window.addEventListener('DOMContentLoaded', handleDOMContentLoaded, false);
