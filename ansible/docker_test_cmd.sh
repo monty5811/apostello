ed5b3d7c63b7f8520d65724b5d0d83872af7ba80
Dean Montgomery <montgomery.dean97@gmail.com>
2018-11-02 17:55:21 +0000

    update to python 3.7

--- ansible/docker_test_cmd.sh
+++ ansible/docker_test_cmd.sh
@@ -7,7 +7,7 @@ ansible-playbook -i 127.0.0.1, test.yml --connection=local
 # check if login page is there
 curl 127.0.0.1/config/first_run/ --fail
 # check django-q is working
-cd /webapps/apostello_py36
+cd /webapps/apostello_py37
 . bin/activate
 . bin/postactivate
 cd apostello
